397f937498c8b5d9541d428a6836a0a5ecedb24a
Yuta <ezochi66@gmail.com>
2024-01-25 22:22:45 +0900

    名前を姓名別々に変更

--- src/main/java/com/nogu66/payroll/Employee.java
+++ src/main/java/com/nogu66/payroll/Employee.java
@@ -10,20 +10,40 @@ import java.util.Objects;
 class Employee {
     private @Id
     @GeneratedValue Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String role;
 
     Employee() {
     }
 
-    Employee(String name, String role) {
-        this.name = name;
+    Employee(String firstName, String lastName, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
     }
 
     // nameを返すメソッド
     public String getName() {
-        return this.name;
+        return this.firstName + " " + this.lastName;
+    }
+
+    // newをfirsNameとlastNameからセットする
+    public void setName(String name) {
+        String[] parts = name.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+    }
+
+
+    // firstNameを返すメソッド
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    // lastNameを返すメソッド
+    public String getLastName() {
+        return this.lastName;
     }
 
     // roleを返すメソッド
@@ -36,9 +56,14 @@ class Employee {
         return this.id;
     }
 
-    // nameをセットするメソッド
-    public void setName(String name) {
-        this.name = name;
+    // firstNameをセットするメソッド
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // lastNameをセットするメソッド
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     // roleをセットするメソッド
@@ -58,17 +83,18 @@ class Employee {
         if (!(o instanceof Employee))
             return false;
         Employee employee = (Employee) o;
-        return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name)
-                && Objects.equals(this.role, employee.role);
+        return Objects.equals(this.id, employee.id) && Objects.equals(this.firstName, employee.firstName)
+                && Objects.equals(this.lastName, employee.lastName) && Objects.equals(this.role, employee.role);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.id, this.name, this.role);
+        return Objects.hash(this.id, this.firstName, this.lastName, this.role);
     }
 
     @Override
     public String toString() {
-        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+        return "Employee{" + "id=" + this.id + ", firstName='" + this.firstName + '\'' + ", lastName='" + this.lastName
+                + '\'' + ", role='" + this.role + '\'' + '}';
     }
 }
